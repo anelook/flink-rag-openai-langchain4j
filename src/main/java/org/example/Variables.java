3e79e4fd11ed6e64c69153304edf360e176b2394
Olena Kutsenko <olena.kutsenko@aiven.io>
2024-08-25 13:03:34 +0200

    first commit

--- src/main/java/org/example/Variables.java
+++ src/main/java/org/example/Variables.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Variables {
 
-    // You can use "demo" api key for demonstration purposes.
-    // You can get your own OpenAI API key here: https://platform.openai.com/account/api-keys
     public static final String OPENAI_API_KEY = "";
 
     public static final String OPENSEARCH_HOST = "";
